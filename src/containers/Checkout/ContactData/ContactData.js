2899bf4dddcad13c11a69d59a4c1e91c9bbd6518
Andy <andrewpeliza@hotmail.com>
2018-10-16 21:24:40 +0100

    Form validation complete and disabled form button when not valid.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,8 +85,11 @@ class ContactData extends Component {
             ]
           },
           value: 'fastest',
+          validation: {},
+          valid: true
         },
       },
+      formIsValid: false,
       loading: false
     }
 
@@ -141,8 +144,13 @@ class ContactData extends Component {
       updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
       updatedFormElement.touched = true;
       updatedOrderForm[inputIdentifier] = updatedFormElement;
-      console.log(updatedFormElement);
-      this.setState({orderForm: updatedOrderForm});
+
+      let formIsValid = true;
+      for (let inputIdentifier in updatedOrderForm) {
+        formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+      }
+
+      this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
     }
 
     render() {
@@ -166,7 +174,7 @@ class ContactData extends Component {
                   invalid={!formElement.config.valid}
                   changed={(event) => this.inputChangedHandler(event, formElement.id)} />
             ))}
-            <Button btnType="Success">ORDER</Button>
+            <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
           </form>
       );
       if (this.state.loading) {
