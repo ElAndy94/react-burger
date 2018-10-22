92b10c7e64bba5019635e0b8debdfb13794926e0
Andy <andrewpeliza@hotmail.com>
2018-10-22 11:21:58 +0100

    Removed logs to console and some little fixes. (Cleanup)

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { withRouter } from 'react-router-dom';
 
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
@@ -27,4 +26,3 @@ const burger = ( props ) => {
 };
 
 export default burger;
-// export default withRouter(burger);
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 
 import ContactData from './ContactData/ContactData';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-import * as actions from '../../store/actions/actionTypes';
 
 class Checkout extends Component {
 
